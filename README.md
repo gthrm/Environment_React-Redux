935810c1d9f1e7cf5b2402feddbc070884848182
cdROma <mrv24ru@yandex.ru>
2018-10-22 11:05:37 +0300

    redux and webpack

---
+++ README.md
@@ -0,0 +1,1 @@
+# Environment-React-Redux
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+};
