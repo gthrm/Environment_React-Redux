e3d90b2aa3eb92c21da3c7ed68f033f087e36684
cdROma <mrv24ru@yandex.ru>
2018-10-22 22:06:35 +0300

    React Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,34 @@
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/main.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
   },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/public',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
+    contentBase: './public',
+    port: 3000,
+    hot: true
   }
 };
